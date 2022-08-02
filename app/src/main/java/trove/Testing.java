21aef73887e25e9c1775d9138ac076d1d7a6835a
Gabriel Morales <moralg640@gmail.com>
2022-08-02 17:34:17 +0000

    Rename

--- app/src/main/java/trove/Testing.java
+++ app/src/main/java/trove/Testing.java
@@ -1,12 +1,12 @@
 package trove;
 
-public class App {
+public class Test {
 
     public static void main(String[] args) {
         Radians rad = new Radians(java.lang.Math.PI);
         Degrees deg = new Degrees(180.0);
 
         System.out.println("Rad pure: " + rad.getMeasure() + " convert: " + rad.toDegrees().getMeasure());
-        System.out.println("Rad pure: " + deg.getMeasure() + " convert: " + deg.toRadians().getMeasure());
+        System.out.println("Deg pure: " + deg.getMeasure() + " convert: " + deg.toRadians().getMeasure());
     }
 }
