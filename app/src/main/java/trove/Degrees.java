7e86b8bf81e80eae7a366e51d8a297c107aed5b4
Gabriel Morales <moralg640@gmail.com>
2022-08-02 13:08:08 +0000

    Renamed

---
+++ app/src/main/java/trove/Degrees.java
@@ -0,0 +1,12 @@
+package trove;
+import static java.lang.Math.PI;
+
+public class Degrees extends Angle {
+	public Degrees(double degrees){
+		super(degrees);
+	}
+
+	public Radians toRadians(){
+		return new Radians(this.measure / 180 * PI);
+	}
+}
