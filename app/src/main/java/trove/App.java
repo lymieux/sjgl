73fd29f0936b758e79baaee431b1c34d0fd19530
Gabriel Morales <moralg640@gmail.com>
2022-08-02 17:31:21 +0000

    Test angle

--- app/src/main/java/trove/App.java
+++ app/src/main/java/trove/App.java
@@ -1,11 +1,12 @@
 package trove;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        Radians rad = new Radians(java.lang.Math.PI);
+        Degrees deg = new Degrees(180.0);
+
+        System.out.println("Rad pure: " + rad.getMeasure() + " convert: " + rad.toDegrees().getMeasure());
+        System.out.println("Rad pure: " + deg.getMeasure() + " convert: " + deg.toRadians().getMeasure());
     }
 }
