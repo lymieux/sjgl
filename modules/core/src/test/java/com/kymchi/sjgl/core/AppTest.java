38796b867478d3f2fbe072728ef5be9d5cece200
moral-g <moralg640@gmail.com>
2022-08-25 04:58:40 -0400

    Commented out b/c based on junit test

--- modules/core/src/test/java/com/kymchi/sjgl/core/AppTest.java
+++ modules/core/src/test/java/com/kymchi/sjgl/core/AppTest.java
@@ -1,20 +1,20 @@
 package com.kymchi.sjgl.core;
 
-import static org.junit.Assert.assertTrue;
+// import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+// import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
-}
+// /**
+//  * Unit test for simple App.
+//  */
+// public class AppTest 
+// {
+//     /**
+//      * Rigorous Test :-)
+//      */
+//     @Test
+//     public void shouldAnswerWithTrue()
+//     {
+//         assertTrue( true );
+//     }
+// }
